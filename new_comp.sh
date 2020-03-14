0c5189f8308d6091f9f624b23ba0f9211a8cd621
Nate Fischer <ntfschr@google.com>
2020-03-14 01:25:36 -0700

    Don't run untrusted code in new_comp.sh
    This changes new_comp.sh to not run untrusted code. Instead, it
    downloads it to a local directory (which is added to ".gitignore"). This
    also includes a list of suggested apt/pip/npm packages to install,
    rather than actually installing them.

--- new_comp.sh
+++ new_comp.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 
-## Run this script for installing this on a new unix system.
-
 cd "$(dirname "$0")" # make sure we're in the correct directory
 
-# Apt-get dependencies
-aptitude_packages=( nvim git git-extras tmux xbindkeys zsh z )
+DIR="install-this-stuff"
+mkdir "$DIR"
+cd "$DIR"
 
-if [ -z "$CI" ]; then
-  echo "This script requires root priviledge"
-  sudo apt-get update &&
-  sudo apt-get upgrade -y &&
-  (
-    set -e
-    for dep in "${aptitude_packages[@]}"; do
-      echo "apt-get installl ${dep}"
-      sudo apt-get install -y "${dep}" >/dev/null
-    done
-  ) || echo "failed on apt-get command" >&2
-fi
-
-# Install node version manager
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash ||
-  echo "nvm installation failed" >&2
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh > install-nvm.sh
 
 # Install zplug
-curl -sL zplug.sh/installer | zsh || echo 'zplug installation failed' >&2
+curl -sL zplug.sh/installer  > install-zplug.zsh
 
-# Install tmux plugin manager
-TPM_DIR="$HOME/.tmux/plugins/tpm"
-if [ -d "${TPM_DIR}" ]; then
-  echo "TPM already installed at ${TPM_DIR}, skipping..."
-else
-  git clone https://github.com/tmux-plugins/tpm "${TPM_DIR}" ||
-    echo 'tpm installation failed' >&2
-fi
+TMUX_PLUGIN_DIR="$HOME/.tmux/plugins/tpm"
+cat > install-tmux-plugin-manager.sh <<END
+#!/bin/bash
+
+# Before you run this, double-check if tmux plugin manager is already cloned
+# there! You can use vim's 'gf' command to jump to the folder if it exists.
+git clone https://github.com/tmux-plugins/tpm "${TMUX_PLUGIN_DIR}"
+END
 
 mkdir -p "$HOME/packages"
 
-which npm &>/dev/null || echo 'Please install node and npm' >&2
+cat<<END
+This script no longer installs things for you, since that usually requires
+running untrusted code. Instead, this will download the code for you to review
+and then execute.
 
-echo ''
-echo 'done'
+Look inside the "install-this-stuff" folder.
+
+Consider the following apt packages:
+ - git git-extras tmux zsh python3-pip python-pip
+ - nodejs npm (or, install them yourself)
+ - nvim (or, build it yourself)
+ - nmap
+ - curl
+ - gawk (hidden dependency of zplug)
+ - arc-theme gnome-tweak-tool
+ - xclip
+
+Consider the following pip packages:
+ - pip3 install --user pynvim
+
+Consider the following npm packages:
+ - npm install -g diff-so-fancy
+ - npm install -g clipbaord-cli
+
+Install npm packages in ~/bin/
+ - cd ~/bin && npm install
+
+END
