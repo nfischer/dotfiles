12ece4512ff636a1bab3aed1686ab7a84295270f
Nate Fischer <ntfschr@google.com>
2021-09-30 20:55:00 -0700

    feat: improve backup script
    These are some changes to my personal backup script.

---
+++ bin/replicate-backup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# A simple script to do replicate a backup on another drive
+# Usage: replicate-backup {path/to/backup/folder} {path/to/other/drive}
+
+set -e
+
+original_backup="$1"
+original_backup="${original_backup%%/}" # remove trailing slash
+other_drive="$2"
+other_drive="${other_drive%%/}" # remove trailing slash
+if [ -z "${original_backup}" ] || [ -z "${other_drive}" ]; then
+  echo "Please pass 2 args:" >&2
+  echo "  replicate-backup {path/to/backup/folder} {path/to/other/drive}" >&2
+  exit 1
+fi
+if [ ! -d "${original_backup}" ] ; then
+  echo "Original backup (1st arg) must exist!" >&2
+  exit 2
+fi
+if [ ! -d "${other_drive}" ] ; then
+  echo "Other drive (2nd arg) must exist!" >&2
+  exit 2
+fi
+
+readonly backup_folder_name="${original_backup##*/}"
+readonly output_dir="${other_drive}/${backup_folder_name}/"
+
+# Must have trailing slash
+echo "Copy ${original_backup} up into '${output_dir}'"
+read -p "Press enter to continue (ctrl-c to quit)" dummyvar
+
+# If this fails, it indicates some bad state
+mkdir "${output_dir}"
+
+backup_start="$(date +%s)"
+rsync --info=progress2 -av --exclude-from="$EXCLUDE_LIST" \
+  "$original_backup/" \
+  "${output_dir}"
+backup_end="$(date +%s)"
+
+runtime=$((backup_end-backup_start))
+
+echo ""
+cat "${output_dir}/BACKUP_METADATA.txt"
+
+echo ""
+echo "Done"
--- bin/rsync-backup.sh
+++ bin/rsync-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Do a simple backup
-# Usage: rsync-backup /path/to/external/drive/new-folder
+# A simple script to do a backup
+# Usage: rsync-backup
 
 set -e
 
@@ -11,15 +11,49 @@ readonly EXCLUDE_LIST="$HOME/backup-exclude-list.txt"
 
 readonly host="$(hostname)"
 readonly date="$(date -I)"
-readonly output_dir="/media/$USER/$DRIVE_NAME/${host}-${date}/"
+
+find_drive() {
+  local -a drives=($(ls "/media/$USER/"))
+  if [[ "${#drives[@]}" < 1 ]]; then
+    echo "Could not find any drives at /media/$USER/" >&2
+    return 1
+  elif [[ "${#drives[@]}" == 1 ]]; then
+    echo "${drives[0]}"
+  else
+    # TODO(nfischer): this should really support this case...
+    echo "Found too many drives: ${drives[@]}" >&2
+    return 2
+  fi
+}
+
+drive_name="$(find_drive)"
+output_dir="/media/$USER/$drive_name/${host}-${date}/"
 
 # Must have trailing slash
 echo "Backup up into '${output_dir}'"
-read -p "Press enter to coninue (ctrl-c to quit)" dummyvar
+read -p "Press enter to continue (ctrl-c to quit)" dummyvar
 
 # If this fails, it indicates some bad state
 mkdir "${output_dir}"
 
+backup_start="$(date +%s)"
 rsync --info=progress2 -av --exclude-from="$EXCLUDE_LIST" \
   "$HOME/" \
   "${output_dir}"
+backup_end="$(date +%s)"
+
+runtime=$((backup_end-backup_start))
+
+log_metadata() {
+  echo "$@" >> "${output_dir}/BACKUP_METADATA.txt"
+}
+log_metadata "Backed up on $(date)"
+log_metadata "Size: $(du -sh "${output_dir}")"
+log_metadata "Time to backup: ${runtime} seconds"
+log_metadata "Top-level files and folders: $(ls ${output_dir})"
+
+echo ""
+cat "${output_dir}/BACKUP_METADATA.txt"
+
+echo ""
+echo "Done"
